baddc03613a83af6589f7a40bc939cef5aa74c1e
Guilherme Cadilhe <guilhermegcadilhe@gmail.com>
2024-04-22 11:40:31 -0300

    New Fields for Status API and SQL Injection Test

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,9 +3,34 @@
 import database from '/infra/database.js'
 
 async function status(req, res) {
-  const result = await database.query('SELECT 1 + 1 as sum;')
-  console.log('result', result.rows)
-  res.status(200).json({ status: 'ok' })
-}
+  const databaseName = process.env.POSTGRES_DB
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query('SHOW server_version;');
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionResult = await database.query('SHOW max_connections;');
+  const databaseMaxConnectionValue = databaseMaxConnectionResult.rows[0].max_connections;
+
+  const databaseOpenConnectionsResult = await database.query({
+    text: `SELECT count(*)::int AS opened_connections FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName]
+  },)
+  // const databaseOpenConnectionsResult = await database.query('SELECT count(*)::int AS opened_connections FROM pg_stat_activity WHERE datname = current_database();')
+  const currentConnections = databaseOpenConnectionsResult.rows[0].opened_connections
+
+  // console.log('currentConnections', currentConnections)
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionValue),
+        opened_connections: currentConnections
+      }
+    }
+  });
+};
 
 export default status
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,18 @@
 test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status")
-  expect(response.status).toBe(200)
-})
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log('responseBody', responseBody)
+  expect(responseBody.updated_at).toBeDefined();
+  // confirma que é um valor de tempo válido 
+  expect(responseBody.updated_at).toEqual(new Date(responseBody.updated_at).toISOString())
+  expect(responseBody.dependencies.database.version).toEqual("16.2")
+  expect(responseBody.dependencies.database.max_connections).toEqual(100)
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1)
+
+});
+
+// test.only("Teste de SQL Injection", async () => {
+//   await fetch("http://localhost:3000/api/v1/status?databaseName=; SELECT pg_sleep(10); --");
+// });
