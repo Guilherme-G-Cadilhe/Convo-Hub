5d4e13e01586aa3bf94b84e93f3ad53dc8355228
Guilherme Cadilhe <guilhermegcadilhe@gmail.com>
2024-05-07 09:25:38 -0300

    Small Env Fixes

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(queryString) {
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === 'development' ? false : true  
+    ssl: getSSLValues()
   })
   try {
     await client.connect()
@@ -25,4 +25,13 @@ async function query(queryString) {
 
 export default {
   query
+}
+
+function getSSLValues() {
+  if(process.env.POSTGRES_CA) {
+    return {
+      ca:process.env.POSTGRES_CA
+    }
+  }
+  return process.env.NODE_ENV === 'development' ? false : true  
 }
