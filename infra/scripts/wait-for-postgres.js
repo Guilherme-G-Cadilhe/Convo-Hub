9f18da9d4545872591616807921766c639209715
Bobnini <guilhermegcadilhe@gmail.com>
2025-09-22 16:10:36 -0300

    feat: adds `wait-for-posgres.js` script

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,20 @@
+const { exec } = require("node:child_process") // Permite executar comandos externos
+
+function checkPostgres() {
+  // Verifica se o postgres esta pronto e se a conexão TCP IP estiver aberta
+  exec('docker exec postgres-dev pg_isready --host localhost', handleReturn)
+
+  function handleReturn(error, stdout, stderr) {
+    // resultado do standard output (saída padrão)
+    // Verifica se string contém "accepting connections"
+    if (stdout.search("accepting connections") !== -1) {
+      console.log("\n[Postgres is ready!]\n");
+      return;
+    }
+    process.stdout.write(".");
+    setTimeout(checkPostgres, 1000)
+  }
+}
+
+process.stdout.write("\n\n[Waiting for postgres]:")
+checkPostgres()
